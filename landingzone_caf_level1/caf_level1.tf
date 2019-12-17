b5577bf54590865f083fb3de5dea0b433afcb47b
Brett Odgers-Jewell <brett.odgers-jewell@avanade.com>
2019-12-17 11:42:01 +0800

    updated local

--- landingzone_caf_level1/caf_level1.tf
+++ landingzone_caf_level1/caf_level1.tf
@@ -6,7 +6,7 @@ module "blueprint_virtual_network" {
     resource_groups_hub                = var.resource_groups_hub
     tags_hub                           = var.tags_hub
     diagnostics_map                    = var.diagnostics_map
-    log_analytics_workspace            = var.log_analytics_workspace
+    log_analytics_workspace            = local.log_analytics_workspace
     diagnostics_settings               = var.diagnostics_settings
     solution_plan_map                  = var.solution_plan_map
     networking_object                  = var.networking_object
