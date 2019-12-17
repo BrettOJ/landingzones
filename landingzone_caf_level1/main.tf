9c971da894cfc246d41cd321280f6fed072db049
Brett Odgers-Jewell <brett.odgers-jewell@avanade.com>
2019-12-17 11:29:02 +0800

    added module

--- landingzone_caf_level1/main.tf
+++ landingzone_caf_level1/main.tf
@@ -21,8 +21,8 @@ data "terraform_remote_state" "landingzone_caf_foundations" {
 
 locals {
     prefix                  = data.terraform_remote_state.landingzone_caf_foundations.outputs.prefix
-    blueprint_foundations   = data.terraform_remote_state.landingzone_caf_foundations.outputs.blueprint_foundations
+    #log_analytics_workspace = data.terraform_remote_state.landingzone_caf_foundations.outputs.blueprint_foundations.log_analytics_workspace
 }
 
-log_analytics_workspace     = local.blueprint_foundations.value.log_analytics_workspace
+
 
