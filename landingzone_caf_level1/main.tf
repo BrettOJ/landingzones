58e6ae880d9d6c3675ff2e577347d969cc77ef94
Brett Odgers-Jewell <brett.odgers-jewell@avanade.com>
2019-12-20 11:32:23 +0800

    revert tfstate

--- landingzone_caf_level1/main.tf
+++ landingzone_caf_level1/main.tf
@@ -13,7 +13,7 @@ data "terraform_remote_state" "landingzone_caf_foundations" {
     config = {
     storage_account_name  = var.lowerlevel_storage_account_name
     container_name        = var.lowerlevel_container_name 
-    key                   = "landingzone_caf_foundations"
+    key                   = var.lowerlevel_key
     resource_group_name   = var.lowerlevel_resource_group_name
   }
 }
@@ -23,6 +23,7 @@ locals {
     tags                    = data.terraform_remote_state.landingzone_caf_foundations.outputs.tags
     blueprint_foundations   = data.terraform_remote_state.landingzone_caf_foundations.outputs.blueprint_foundations 
     log_analytics_workspace = data.terraform_remote_state.landingzone_caf_foundations.outputs.log_analytics_workspace
+
     
 }
 
