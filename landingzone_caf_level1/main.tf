7df42ca9ea80a9479d8b53b5ca5270c294f01fe2
Brett Odgers-Jewell <brett.odgers-jewell@avanade.com>
2019-12-20 10:58:35 +0800

    found scrip issue

--- landingzone_caf_level1/main.tf
+++ landingzone_caf_level1/main.tf
@@ -13,17 +13,16 @@ data "terraform_remote_state" "landingzone_caf_foundations" {
     config = {
     storage_account_name  = var.lowerlevel_storage_account_name
     container_name        = var.lowerlevel_container_name 
-    key                   = var.lowerlevel_key
+    key                   = "landingzone_caf_foundations"
     resource_group_name   = var.lowerlevel_resource_group_name
   }
 }
 
 locals {
-    prefix                  = data.terraform_remote_state.landingzone_caf_foundations.outputs.deployment_msi
-    tags                    = data.terraform_remote_state.landingzone_caf_foundations.outputs.deployment_msi
-    blueprint_foundations   = data.terraform_remote_state.landingzone_caf_foundations.outputs.deployment_msi
-    #log_analytics_workspace = data.terraform_remote_state.landingzone_caf_foundations.outputs.log_analytics_workspace
-    log_analytics_workspace = local.blueprint_foundations ["log_analytics_workspace"]
+    prefix                  = data.terraform_remote_state.landingzone_caf_foundations.output.prefix
+    tags                    = data.terraform_remote_state.landingzone_caf_foundations.outputs.tags
+    blueprint_foundations   = data.terraform_remote_state.landingzone_caf_foundations.outputs.blueprint_foundations 
+    log_analytics_workspace = data.terraform_remote_state.landingzone_caf_foundations.outputs.log_analytics_workspace
     
 }
 
